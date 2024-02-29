130c23a656b6984fc66a051ece5f06ce677199a9
feuzw <feuzw21@gmail.com>
2024-02-29 15:47:38 +0900

    현위치받아오기

---
+++ frontend/src/pages/Main/index.js
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import './main.css'
+
+const { kakao } = window;
+
+const MainPage=()=>{
+
+    useEffect(()=>{
+        const geoLocation=()=>{
+            if(navigator.geolocation){
+                navigator.geolocation.getCurrentPosition((position)=>{
+                    const lat=position.coords.latitude;
+                    const lng=position.coords.longitude;
+
+                    const container=document.getElementById('map');
+                    const options={
+                        center:new kakao.maps.LatLng(lat,lng),
+                        level:3
+                    };
+                    const map = new kakao.maps.Map(container,options);
+                },(error)=>{
+                    console.error("failed : "+error.message);
+                });
+            }else{
+                console.error("Geolocation is not supported bt this browser.");
+            }
+        };
+        geoLocation();
+    },[])
+
+    return(
+        <div>
+            <div id="map"></div>
+        </div>
+    )
+}
+export default MainPage;
