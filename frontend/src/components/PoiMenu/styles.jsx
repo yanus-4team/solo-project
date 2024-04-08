ae6f29f481ea6db9359e2c7dabb8e2c9638dc79c
안수윤 <suyun6022@gmail.com>
2024-04-08 23:46:09 +0900

    풀

--- frontend/src/components/PoiMenu/styles.jsx
+++ frontend/src/components/PoiMenu/styles.jsx
@@ -3,27 +3,28 @@ import styled from "styled-components";
 export const PoiContainer = styled.div`
   position: fixed;
   top: 0;
-  left: -350px; // 기본 상태에서는 화면 왼쪽 바깥에 위치
+  left: -350px;
   width: 300px;
   margin-left: 8px;
   margin-top: 100px;
-  height: 130px;
+  height: 200px;
   border-radius: 30px;
   background-color: #fff;
   transition: left 0.5s ease;
   z-index: 10;
+  box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.2); /* 그림자 추가 */
   &.open {
-    left: 0; // .open 클래스가 있을 때 화면 안으로 슬라이드
+    left: 0;
   }
   @media (max-width: 768px) {
-    width: 300px; // 너비를 화면 너비와 같게 조정
-    left: -350px; // 초기 위치를 화면 너비만큼 왼쪽으로 설정하여 화면 밖으로 이동
-    margin-left: 8px; // 모바일 화면에서는 외부 여백을 제거
-    margin-top: 100px; // 상단 여백 제거
-    height: 130px; // 높이를 자동으로 조정하여 내용에 맞게 변경
-    border-radius: 30px; // 모바일 화면에서는 모서리 둥글기 제거
+    width: 300px;
+    left: -350px;
+    margin-left: 8px;
+    margin-top: 100px;
+    height: 130px;
+    border-radius: 30px;
     &.open {
-      left: 0; // .open 클래스가 있을 때 화면 안으로 슬라이드
+      left: 0;
     }
   }
 `;
@@ -31,8 +32,8 @@ export const PoiContainer = styled.div`
 export const CloseBtn = styled.img`
   position: absolute;
   width: 20px;
-  top: 15px;
-  right: 10px;
+  top: 25px;
+  right: 20px;
   cursor: pointer;
 `;
 
@@ -42,11 +43,12 @@ export const PoiContent = styled.div`
 `;
 
 export const Title = styled.div`
-    margin-left: 105px;
-    margin-bottom: 1rem;
+    margin-left: 100px;
+    margin-bottom: 2rem;
+    margin-top: 50px;
     color: #333;
     font-weight: normal;
-    font-size: 25px;
+    font-size: 30px;
 `
 export const MemberContainer = styled.div`
   display: flex;
